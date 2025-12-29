8d76f0ec66747141d53a8ad49a460433a47eb239
Pancake <13051345080@163.com>
2025-12-29 15:57:05 +0800

    fix test

--- src/api/routers/system.py
+++ src/api/routers/system.py
@@ -174,7 +174,7 @@ async def test_embeddings_connection():
 
         response_time = (time.time() - start_time) * 1000
 
-        if embeddings and len(embeddings) > 0 and len(embeddings[0]) > 0:
+        if embeddings is not None and len(embeddings) > 0 and len(embeddings[0]) > 0:
             return TestResponse(
                 success=True,
                 message="Embeddings connection successful",
